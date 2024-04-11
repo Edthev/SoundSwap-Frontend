32f5aaebbed8437c8080c53d7097bdd725981f32
Edward Vargas <edthevargas@gmail.com>
2024-04-11 15:54:30 -0400

    fixed styling

--- src/components/IndexComponents/LoggedInComponents/LoggedIn.jsx
+++ src/components/IndexComponents/LoggedInComponents/LoggedIn.jsx
@@ -1,8 +1,17 @@
-function LoggedIn() {
+function LoggedIn({ Logo, Company, PrimaryColor, Display }) {
    return (
       <div id="loggedin">
-         <div id="user-profile"> </div>
-         <div id="oauth"> </div>
+         <div id="user-profile">
+            <h3>Display Name: </h3>
+            <h3>{Company} ID: </h3>
+            <h3>Email: </h3>
+            <h3>Link: </h3>
+            <h3>Pfp: </h3>
+         </div>
+         <div id="oauth">
+            <h3>Access Token: </h3>
+            <h3>Refresh Token: </h3>
+         </div>
          <button className="btn btn-default" id="obtain-new-token">
             Obtain new token using the refresh token
          </button>
