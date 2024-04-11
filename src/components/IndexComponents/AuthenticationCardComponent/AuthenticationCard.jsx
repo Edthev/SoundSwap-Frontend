a1acbe4aa4a47bf7fc801eca098bc12e0005530e
Edward Vargas <edthevargas@gmail.com>
2024-04-11 15:53:34 -0400

    once authenticated the login card will now not display

--- src/components/IndexComponents/AuthenticationCardComponent/AuthenticationCard.jsx
+++ src/components/IndexComponents/AuthenticationCardComponent/AuthenticationCard.jsx
@@ -1,20 +1,30 @@
 import "./AuthenticationCard.scss";
 import { Link } from "react-router-dom";
-function AuthenticationCard({ Logo, Company, PrimaryColor }) {
+import LoggedIn from "../LoggedInComponents/LoggedIn";
+function AuthenticationCard({ Logo, Company, PrimaryColor, Display, URL }) {
    return (
-      <div className="container">
-         <img src={Logo} className="logo" />
-         <div id="login">
-            <h1 className="mainText">Authenticate With {Company}:</h1>
-            <Link
-               to={"/login?" + Company}
-               className="btn btn-primary"
-               style={{ backgroundColor: PrimaryColor }}
-            >
-               Log in with {Company}
-            </Link>
+      <>
+         <div
+            className="container"
+            style={Display.includes(Company) ? { display: "none" } : { color: "white" }}
+         >
+            <img src={Logo} className="logo" />
+            <div id="login">
+               <h1 className="mainText">Authenticate With {Company}:</h1>
+               <Link
+                  to={URL}
+                  className="btn btn-primary"
+                  style={{ backgroundColor: PrimaryColor }}
+               >
+                  Log in with {Company}
+               </Link>
+               <p>
+                  Tokens:{Display} {Display == Company}
+               </p>
+            </div>
          </div>
-      </div>
+         <LoggedIn />
+      </>
    );
 }
 
